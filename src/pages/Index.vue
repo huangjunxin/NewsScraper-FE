59df72a2762b6e4f37f2795c3d52f1c77e2beff3
Jixzjeng <huangjunxin@live.cn>
2020-08-24 17:54:29 +0800

    [Feat]: 增加實現fetchJob

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -58,7 +58,7 @@
           :data="resultUrlsData"
           :columns="resultUrlsColumns"
           row-key="link-href"
-          rows-per-page-options="0"
+          :rows-per-page-options="[0]"
           wrap-cells
         />
       </div>
@@ -111,12 +111,13 @@ export default {
   },
   methods: {
     // 單擊 Submit 提交時
-    onSubmit () {
+    async onSubmit () {
+      console.info('[methods][onSubmit]')
       const bar = this.$refs.bar
       bar.start()
       this.isSubmitDisabled = true
       for (const oneNews of this.fetcherListAccepted) {
-        this.$http.get('/urlLists', {
+        await this.$http.get('/urlLists', {
           params: {
             news: oneNews,
             keyword: this.keywords,
@@ -134,20 +135,24 @@ export default {
               for (const row of res.data) {
                 const temp = {
                   ...row,
-                  status: 'waiting'
+                  status: 'waiting',
+                  newsName: oneNews
                 }
                 this.resultUrlsData.push(temp)
-              }
-              // 自動開始進行fetch工作
-              if (!this.isFetchJobStarted) {
-                this.fetchJob()
               }
             }
           })
       }
+      // 自動開始進行fetch工作
+      if (this.resultUrlsData.length > 0) {
+        if (!this.isFetchJobStarted) {
+          this.fetchJob()
+        }
+      }
     },
     // Fetcher List 全選操作
     fetcherListAllSelect (value, evt) {
+      console.info('[methods][fetcherListAllSelect]')
       this.isfetcherListAllSelect = value
       const allList = []
       for (const item of this.fetcherListOptions) {
@@ -160,17 +165,59 @@ export default {
       }
     },
     // Fetch Job 任務分發
-    fetchJob () {
+    async fetchJob () {
+      console.info('[methods][fetchJob]')
       this.isFetchJobStarted = true
-      // while (this.isFetchJobStarted) {
-      //   if (Object.keys(this.fetchJobQueue).length < 5) {
-      //     for (const row of this.resultUrlsData) {
-      //       if (row.status === 'waiting') {
-      //         this.fetchJobQueue[row['link-href']] = row.status
-      //       }
-      //     }
-      //   }
-      // }
+      while (this.isFetchJobStarted) {
+        // 當任務隊列數量少於5時，新增一個分發任務
+        if (Object.keys(this.fetchJobQueue).length < 5) {
+          for (const row of this.resultUrlsData) {
+            if (row.status === 'waiting') {
+              // 分發任務
+              await this.$http.post('/fetchJob', JSON.stringify({
+                url: row['link-href'],
+                newsName: row.newsName
+              }))
+                .then(res => {
+                  if (res.data.code === -1) {
+                    console.error(res.data.data.msg)
+                  } else {
+                    // 更改當前任務在列表的顯示狀態
+                    row.status = 'running'
+                    this.fetchJobQueue[row['link-href']] = row.status
+                  }
+                })
+            }
+          }
+        } else if (Object.keys(this.fetchJobQueue).length === 5) {
+          // 當任務隊列數量等於5時，每隔一秒發送請求更新狀態
+          for (const item in this.fetchJobQueue) {
+            await this.$http.get('/statusJob', {
+              params: {
+                url: item
+              }
+            })
+              .then(res => {
+                if (res.data.code === -1) {
+                  console.error(res.data.data.msg)
+                } else {
+                  if (res.status === 'completed') {
+                    delete this.fetchJobQueue[item]
+                    for (const row of this.resultUrlsData) {
+                      if (row.url === item.toString()) {
+                        row.status = res.status
+                      }
+                    }
+                  }
+                }
+              })
+          }
+          // 等待一秒
+          setTimeout(() => {
+            console.log('Wait a second')
+          }, 1000)
+        }
+      }
     }
   },
   mounted () {
